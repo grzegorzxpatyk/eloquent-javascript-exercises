e681485df1a410a1b545e358e07e9f5517c3400b
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2022-12-30 09:54:15 +0000

    Create loopingTriangle.js & implement printTriangle function

---
+++ src/programStructure/loopingTriangle.js
@@ -0,0 +1,16 @@
+function printTriangle() {
+    const hash = '#';
+    let triangleString = '';
+
+    for(let i = 0; i < 7; i++) {
+        let hashRow = '';
+        for(let j = 0; j <= i; j++) {
+            hashRow += hash;
+        }
+        triangleString += hashRow + '\n';
+    }
+
+    console.log(triangleString);
+}
+
+export default printTriangle;
