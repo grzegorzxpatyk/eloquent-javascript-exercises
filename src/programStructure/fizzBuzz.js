9d51b7412ae2dc6c0e268f7c0be44f06f0f3b9e6
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-01-12 19:16:55 +0000

    fix bugs in index.js and fizzBuzz.js

--- src/programStructure/fizzBuzz.js
+++ src/programStructure/fizzBuzz.js
@@ -3,6 +3,6 @@ export function fizzBuzz() {
     let print = !(i % 3 === 0 || i % 5 === 0) ? i : '';
     if (i % 3 === 0) print += 'Fizz';
     if (i % 5 === 0) print += 'Buzz';
-    return print;
+    console.log(print);
   }
 }
